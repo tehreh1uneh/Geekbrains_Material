2e7cd4bf45cd418925a34ee1025e20ed7963374e
tehreh1uneh <andrey.solovetov@gmail.com>
2018-03-16 21:54:52 +1000

    lesson 7

---
+++ app/src/main/java/com/tehreh1uneh/geekbrains_material/tab/CenterFragment.java
@@ -0,0 +1,27 @@
+package com.tehreh1uneh.geekbrains_material.tab;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.tehreh1uneh.geekbrains_material.R;
+
+public class CenterFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_data, container, false);
+        TextView textView = view.findViewById(R.id.text_view);
+
+        textView.setText("This is a CENTER fragment");
+
+        return view;
+    }
+
+}
